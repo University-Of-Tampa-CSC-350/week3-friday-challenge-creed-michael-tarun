aefbf9aed473f77232914956f41e30ce09e221b7
MichaelRSK <123205751+MichaelRSK@users.noreply.github.com>
2026-02-13 14:27:51 -0500

    FEAT - Task 5 completed when doing 3 and 4, and fragment added for Helicopter Withdraws to add extra level

---
+++ app/src/main/java/com/kancharla/abhilash/fc_003/m1_55000.kt
@@ -0,0 +1,25 @@
+package com.kancharla.abhilash.fc_003
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+
+class m1_55000 : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_m1_55000, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        view.findViewById<View>(R.id.button5).setOnClickListener {
+            findNavController().navigate(R.id.action_m1_55000_to_m1_39462)
+        }
+    }
+}
